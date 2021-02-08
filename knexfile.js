2619512ceb9a8bf97e1224c414337ff2ca6e8cae
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-02-07 18:54:25 -0800

    DB Build

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,11 @@ const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
   migrations: { directory: './data/migrations' },
-  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+  seeds: {
+    directory: './data/seeds'
+  },
+  pool: { afterCreate: (conn, done) => 
+    conn.run('PRAGMA foreign_keys = ON', done) },
 }
 
 module.exports = {
