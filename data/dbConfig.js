77430192adfa082c8f9f4316e2f44638d41b73fc
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-02-08 16:23:03 -0800

    Wont run the server

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -5,4 +5,4 @@ const knex = require('knex');
 const config = require('../knexfile.js');
 
 const environment = process.env.NODE_ENV || "development";
-module.exports = knex(environment.development);
+module.exports = knex(config.development);
--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,45 @@
-// do not make changes to this file (except to optionally add seeds)
-const sharedConfig = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  migrations: { directory: './data/migrations' },
-  seeds: {
-    directory: './data/seeds'
-  },
-  pool: { afterCreate: (conn, done) => 
-    conn.run('PRAGMA foreign_keys = ON', done) },
-}
+// // do not make changes to this file (except to optionally add seeds)
+// const sharedConfig = {
+//   client: 'sqlite3',
+//   useNullAsDefault: true,
+//   migrations: { directory: './data/migrations' },
+//   seeds: {
+//     directory: './data/seeds'
+//   },
+//   pool: { afterCreate: (conn, done) => 
+//     conn.run('PRAGMA foreign_keys = ON', done) },
+// }
+
+// module.exports = {
+//   development: {
+//     ...sharedConfig,
+//     connection: { filename: './data/lambda.db3' },
+//   },
+//   testing: {
+//     ...sharedConfig,
+//     connection: { filename: './data/test.db3' },
+//   },
+// };
+
 
 module.exports = {
   development: {
-    ...sharedConfig,
-    connection: { filename: './data/lambda.db3' },
-  },
-  testing: {
-    ...sharedConfig,
-    connection: { filename: './data/test.db3' },
+    client: 'sqlite3',
+    useNullAsDefault: true, // needed for sqlite
+    connection: {
+      filename: './data/lambda.db3',
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
 };
